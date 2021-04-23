d30cf237b160a0fe2dc72afa6d75ef94c4ed3ebe
Selene ToyKeeper <git@toykeeper.net>
2021-04-23 01:21:24 -0600

    attempt to get TT(layer) and LT(layer, kc) working without any weird timing issues ... succeeded, but broke other features along the way :(

--- keyboards/whitefox/keymaps/toykeeper/config.h
+++ keyboards/whitefox/keymaps/toykeeper/config.h
@@ -1,11 +1,49 @@
 // config.h for ToyKeeper's Whitefox layout
 
-// meh, this doesn't seem to help at all  :(
-#define PERMISSIVE_HOLD
-//#define RETRO_TAPPING
-#define IGNORE_MOD_TAP_INTERRUPT
+// QMK tap-hold behavior doesn't work well for quick actions, so try to fix it
+// Example 1:
+//   - tapping term is 200ms
+//   - two keys next to each other are;
+//     - Fn/Home: LT(3,KC_HOME)
+//     - Tilde/Paste: KC_GRV on base layer, LSFT(KC_INS) on layer 3
+//   - quickly press and release both in under 200ms:
+//     - press Fn
+//     - press Tilde
+//     - release both keys; order can vary
+//   - desired result: Paste (Shift+Insert)
+//   - actual result: varies
+//     - Sometimes does a Paste as desired
+//     - Sometimes types Grave and then Home
+//     - Sometimes types Home and then Grave
+//     - Sometimes does a Paste and Home
+// Example 2:
+//   - one key is Fn (TT(2)) and another is Space
+//   - in layer 2, the Space is Play
+//   - quickly (in less than 200ms) press Fn and Space to toggle music
+//     - press Fn
+//     - press Space
+//     - release both keys, in arbitrary order
+//   - desired result: the Play key event is pressed and released once
+//   - actual result:
+//     - Sometimes presses/releases Play once, as expected.
+//     - Sometimes inserts a Space.
+//     - Sometimes presses Play and releases Space, so the Play key sticks.
+//       (and music stutters)
+//
+// So try to avoid that without losing the ability to overload mod/layer keys.
+//
+// meh, the docs say permissive hold is the answer,
+// but it doesn't seem to help at all  :(
+//#define PERMISSIVE_HOLD
+//#define IGNORE_MOD_TAP_INTERRUPT
+//
+// this works, but causes other issues:
+// - TT(layer) doesn't work
+// - Space Cadet shift keys don't work
+// - can't press+release an overloaded key without sending the "tap" version
+#define RETRO_TAPPING
 #undef  TAPPING_TERM
-#define TAPPING_TERM 150  // default 200
+#define TAPPING_TERM 1  // default 200
 
 ///// make the mouse more usable /////
 // Delay between pressing a movement key and cursor movement
